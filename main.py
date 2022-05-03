706147524b73f50160d93b9259965d1d1634fcf3
NopoTheGamer <noahogno@gmail.com>
2022-05-03 11:09:17 +1000

    more joke

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import random
 
 from guizero import *
 joke1 = ["What do you call a fake noodle?", "what is deez", "why did the crab go to jail", "why did the chicken cross the road", "why did the zak go to the dentist", "what do you call a deer with no eye",
-         "a man walks into a bar and says ouch", "no", ""]
-joke2 = ["An Impasta!", "deez nuts", "crimes", "darcy keeys", "Your mother was a hamster and your father smelt of elderberries", "!", "blind", "bottom text",
-         "vodka", "because it didnt", "after that it went to jail"]
+         "a man walks into a bar and says ouch", "no", "be ye not doing maths here", "amoungus"]
+joke2 = ["An Impasta!", "deez nuts", "crimes", "darcy keeys", "blind", "bottom text",
+         "vodka", "because it didnt", "after that it went to jail", "because it was a chicken", "because it was a zak", "because it was a deer", "because it was a", "among us"]
 
 app = App(title="Joke Generator")
 
